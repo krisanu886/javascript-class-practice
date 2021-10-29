cdf6ac8ca30bce33d0d14f52bafb6129a0d091cc
Krisanu <kisanudey886@gmail.com>
2021-10-29 13:07:49 +0530

    updated

--- practice.js
+++ practice.js
@@ -16,3 +16,31 @@ console.log(name.length);
 //upper case & lower case translate.
 console.log(name.toUpperCase());
 console.log(name.toLowerCase());
+// trim function-- erase all space.
+let e = '                Some content here            ';
+console.log(e);
+console.log(e.trim());
+
+//includes() --function -- searching element. output true/false.
+let krisanu = 'jhbsjibc dssh Krisanu jbjbjsdbib hbijcbidbj jbhihginb';
+let anu = 'Krisanu';
+console.log(krisanu.includes(anu));
+//index posision.
+console.log(anu.charAt(4));
+
+//concat some element.
+console.log(anu.concat(' Dey'));
+//using split method
+let mr = "1,2,3,4,5,6,7,8";
+console.log(mr.split(','));
+
+//sum in two numbers
+let j = 50;
+let k= 2;
+
+console.log(j+k);
+console.log(j-k);
+console.log(j/k);
+console.log(j*k);
+console.log(j%k);
+
