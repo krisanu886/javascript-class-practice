a7edacb80e7c2c3e39a98ab81553f01a6e495a1c
Krisanu <kisanudey886@gmail.com>
2021-10-29 09:08:40 +0530

    practice

---
+++ practice.js
@@ -0,0 +1,18 @@
+let a = 56;
+let n = "krisanu";
+
+console.log(a);
+console.log(n);
+console.log(typeof a);
+console.log(typeof n);
+
+// concat the two variable using "+" operator. & printing.
+let c = 'Krisanu';
+let d= 'Dey';
+let name = c + " "+ d;
+console.log(name);
+//see variable values length.
+console.log(name.length);
+//upper case & lower case translate.
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
